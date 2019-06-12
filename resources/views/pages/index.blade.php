35ef9a94b6f6b455e4f2383adfbea6e3a62539c7
Linards Lazdiņš <linards_lazdins@hotmail.com>
2019-06-12 22:34:41 +0300

    quick fix

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('content')
+ @include('inc.navbar')
+ @include('index.header')
+ @include('index.portfolio')
+ @include('index.about-me')
+ @include('index.contact-me')
+@stop
