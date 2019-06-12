5dfda0a7698d2dd80dd51ebce5cc1b7a849eb60e
Linards Lazdiņš <linards_lazdins@hotmail.com>
2019-06-12 22:32:00 +0300

    optimization for blade template usage

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('content')
+ @include('inc.navbar')
+ @include('index.header')
+ @include('indexs.portfolio')
+ @include('index.about-me')
+ @include('index.contact-me')
+@stop
