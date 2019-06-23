4bf57c95b22707957b07e4eea494f9336f65f30b
Linards Lazdiņš <linards_lazdins@hotmail.com>
2019-06-23 13:32:32 +0300

    change to front page portfolio section

--- resources/views/index/portfolio.blade.php
+++ resources/views/index/portfolio.blade.php
@@ -5,68 +5,16 @@
          <div class="underline"></div>
      </div>
      <div class="row justify-content-center">
-         <div class="col-lg-4 col-md-4 col-sm-6 mx-auto item">
+         <div class="col-8 mx-auto item">
              <div class="card border-0 card-shadow">
                  <a class="lightbox" href="gallery/">
                      <img class="card-img img-fluid img-blur" src="img/black-white/0206_cover_1200px.jpg">
-                     <div class="card-img-overlay">
-                         <h5 class="text-white text-uppercase font-weight-bold p-2 portfolio-heading">Black&White</h5>
-                     </div>
-                 </a>
-
-             </div>
-         </div>
-         <div class="col-lg-4 col-md-4 col-sm-6 mx-auto item">
-             <div class="card border-0 card-shadow">
-                 <a class="lightbox" href="gallery/">
-                     <img class="card-img img-fluid img-blur" src="img/creative/01_cover_1200px.jpg">
-                     <div class="card-img-overlay">
-                         <h5 class="text-white text-uppercase font-weight-bold p-2 portfolio-heading">Creative</h5>
-                     </div>
                  </a>
              </div>
+             <div class="all-photos-btn text-align-center">
+                <a class="text-dark text-uppercase font-weight-bold anime-border portfolio-link pt-3" href="gallery/">see all photos</a>
+            </div>
          </div>
-         <div class="col-lg-4 col-md-4 col-sm-6 mx-auto item">
-             <div class="card border-0 card-shadow">
-                 <a class="lightbox" href="gallery/">
-                     <img class="card-img img-fluid img-blur" src="img/latvia/01_1200px_cover.jpg">
-                     <div class="card-img-overlay">
-                         <h5 class="text-white text-uppercase font-weight-bold p-2 portfolio-heading">Latvia</h5>
-                     </div>
-                 </a>
-             </div>
-         </div>
-         <div class="col-lg-4 col-md-4 col-sm-6 mx-auto item">
-             <div class="card border-0 card-shadow">
-                 <a class="lightbox" href="gallery/">
-                     <img class="card-img img-fluid img-blur" src="img/pattern/01_cover_1200px.jpg">
-                     <div class="card-img-overlay">
-                         <h5 class="text-white text-uppercase font-weight-bold p-2 portfolio-heading">Patterns</h5>
-                     </div>
-                 </a>
-             </div>
-         </div>
-         <div class="col-lg-4 col-md-4 col-sm-6 mx-auto item">
-             <div class="card border-0 card-shadow">
-                 <a class="lightbox" href="/gallery/">
-                     <img class="card-img img-fluid img-blur" src="img/streetphoto/01_cover_1200px.jpg">
-                     <div class="card-img-overlay">
-                         <h5 class="text-white text-uppercase font-weight-bold p-2 portfolio-heading">Street Photo</h5>
-                     </div>
-                 </a>
-             </div>
-         </div>
-         <div class="col-lg-4 col-md-4 col-sm-6 mx-auto item">
-             <div class="card border-0 card-shadow">
-                 <a class="lightbox" href="gallery/">
-                     <img class="card-img img-fluid img-blur" src="img/yellowstone/01_cover_1200px.jpg">
-                     <div class="card-img-overlay">
-                         <h5 class="text-white text-uppercase font-weight-bold p-2 portfolio-heading">Yellowstone</h5>
-                     </div>
-                 </a>
-             </div>
-         </div>
-
      </div>
  </div>
 </section>
