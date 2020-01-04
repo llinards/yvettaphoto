74213ceac292e878558bfd765a336a9a99f0a5d1
Linards <linards_lazdins@hotmail.com>
2020-01-05 00:02:08 +0200

    spam protection for the form

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Spatie\Honeypot\ProtectAgainstSpam;
 
 class Kernel extends HttpKernel
 {
@@ -19,6 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Spatie\Honeypot\ProtectAgainstSpam::class,
     ];
 
     /**
