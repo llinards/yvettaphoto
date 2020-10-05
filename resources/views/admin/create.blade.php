3683c70cca217361f93498b98cdbb35553c29959
Linards <linards_lazdins@hotmail.com>
2020-10-05 20:18:30 +0300

    option to change header background image

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.admin-default',['title' => 'Jauna titulbilde'])
+@section('content')
+    <div class="container mt-4">
+        @include('inc.status-messages')
+        <div class="jumbotron">
+            <div class="d-flex flex-column align-items-center">
+                <h2 class="admin__headings">Mainīt titulbildi</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center m-2">
+            <form action="/admin/titulbilde/jauna" enctype="multipart/form-data" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="main-img-cover" class="col-form-label">Titulbilde</label>
+                    <input type="file" class="form-control-file" name="main-img-cover" id="main-img-cover">
+                    <small class="form-text text-muted"><strong>Bildes
+                            izmēram jābūt pēc iespējas mazākam.</strong><br /> To samazināt var <a href="https://tinypng.com"
+                                                                                             target="_blank">šajā
+                            lapā</a>.</small>
+                    @error('main-img-cover')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-success">Mainīt</button>
+                <a class="btn btn-secondary" href="/admin/kategorijas">Atpakaļ</a>
+            </form>
+        </div>
+    </div>
+@endsection
