05df9d62ad5aa506af65d8ab1b00e7d8987b430b
LINARDS L <linards_lazdins@hotmail.com>
2021-07-29 16:05:36 +0300

    created user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'linards',
+            'email' => 'linards@linards.com',
+            'password' => Hash::make('password')
+        ]);
     }
 }
