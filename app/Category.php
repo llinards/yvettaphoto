a6a062c9fb20bb0b2b41c97925e6edf75069afc4
LINARDS L <linards_lazdins@hotmail.com>
2021-08-15 14:40:58 +0300

    Updating Categories controller

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name','description','category_slug','cover_photo_url'];
+
     public function image()
     {
         return $this->hasMany(Image::class);
