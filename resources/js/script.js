b1171200c9e34b2264a0770d01fb5ca26c81ee99
LINARDS L <linards_lazdins@hotmail.com>
2021-10-10 19:26:08 +0300

    menu in works

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,6 @@
 // change background for navbar when scrolling down
 $(function() {
-  
+
   $(document).scroll(function() {
     var $nav = $(".fixed-top");
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
