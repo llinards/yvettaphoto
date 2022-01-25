89d701a60cb41d8955570bed0f8ea85d4fccf8aa
LINARDS L <linards_lazdins@hotmail.com>
2022-01-25 16:12:57 +0200

    Link update

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -13,7 +13,7 @@
           <div class="col">
             <p class="lead main-text">The photo “Wild” has been selected by juror, Susan Burnstine.  It will be included in the exhibition “All About the Light” at The SE Center for Photography.</p>
             <p class="lead main-text"><a href="https://www.sec4p.com/new-index" target="_blank">www.sec4p.com</a></p>
-            <p class="lead main-text">To visit full project “Spring” please visit <a href="https://yvettaphoto.com/portfolio">portfolio</a>.</p>
+            <p class="lead main-text">To visit full project “Spring” please visit <a href="https://yvettaphoto.com/portfolio/spring">portfolio</a>.</p>
             <p class="lead main-text">All About the Light will open Friday, March 4, and remain open through March.</p>
             <div class="text-center mt-5">
               <img src="../img/news/wild.jpg" width="350" class="img-fluid" alt="">
