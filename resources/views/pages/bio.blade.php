d0ca9a54191f8b5ab1c2493e145b18c09a01f0ec
LINARDS L <linards_lazdins@hotmail.com>
2022-07-22 14:11:48 +0300

    Updated content.

--- resources/views/pages/bio.blade.php
+++ resources/views/pages/bio.blade.php
@@ -13,16 +13,16 @@
           <div class="main-text-img text-center mb-5">
             <img src="../img/iveta-lazdina-artist-statement.jpeg" width="250" class="img-fluid" alt="">
           </div>
-          <p class="lead main-text">Iveta Lazdina is a Latvia-based visual artist. She is best known for her dreamy and imagining photographs of nature. Her fine artworks consist of abstract and semi-abstract photographs created via ICM and multi-exposure photographic techniques to evoke and let us feel the magic connection with natural processes and rhythms. The photographs are visual mediation processes.</p>
-          <p class="lead main-text mb-3">She aims to explore inner emotional experience to find an inner balance between being and reflection to the resonance of expression of lights, shapes, and textures.</p>
-          <p class="lead main-text mb-3">Lazdina started fundamental knowledge in Photo Academia in Riga (Latvia). Since 2019 she has focused on creative technics via studies in Masterclasses in Europe and US. Lazdina  has studied art mainly concentrated on impressionism and expressionism concepts and technic.  Lazdina has a Master’s degree in environmental science and long experience in managerial positions.</p>
-          <p class="lead main-text mb-3">Her photography has been exhibited nationally and internationally:</p>
-          <ul>
-            <li class="lead main-text">SE Center for Photography, Grenville, SC</li>
-            <li class="lead main-text">Photo Place Gallery, Middlebury, VT, with Jurors AWARD  nomination</li>
-            <li class="lead main-text">ASMITH Gallery, Johnson City, TX.</li>
-          </ul>
-          <p class="lead main-text mb-3">Lazdina's work has been published online in the ICMPhoto Magazine, LeMag Magazine.</p>
+          <p class="lead main-text">Iveta Lazdina is a fine art photographer from Latvia. Her interest in photography was raised while traveling to embed personal feelings of particular moments, moving beyond the medium's ability to reproduce reality. Iveta is inspired by abstract painters, and she works in an abstract and semi-abstract manner with ICM and multi-exposure technics. Her works are drawn to find harmony and balance between the inner and outer world.</p>
+          <p class="lead main-text mb-3">She started her fundamental knowledge in photography in Photo Academie in Riga (Latvia). In addition, Iveta has studied art in several creative classes and International workshops.</p>
+          <p class="lead main-text mb-3">Lazdina's images have been exhibited in several group exhibitions and published several times in ICMPhoto Magazine, LeMag online Magazine.</p>
+{{--          <p class="lead main-text mb-3">Her photography has been exhibited nationally and internationally:</p>--}}
+{{--          <ul>--}}
+{{--            <li class="lead main-text">SE Center for Photography, Grenville, SC</li>--}}
+{{--            <li class="lead main-text">Photo Place Gallery, Middlebury, VT, with Jurors AWARD  nomination</li>--}}
+{{--            <li class="lead main-text">ASMITH Gallery, Johnson City, TX.</li>--}}
+{{--          </ul>--}}
+{{--          <p class="lead main-text mb-3">Lazdina's work has been published online in the ICMPhoto Magazine, LeMag Magazine.</p>--}}
           <div class="submit-btn pt-0 text-center mb-3">
             <button class="btn" id="read-more-bio-btn" type="button" data-toggle="collapse" data-target="#read-more-bio" aria-expanded="false"
               aria-controls="read-more-bio">
