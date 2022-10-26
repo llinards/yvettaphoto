0b64b1489cbee725d42636034ccdc8080114f95b
LINARDS L <linards_lazdins@hotmail.com>
2022-10-26 19:39:41 +0300

    Added title field and updated layout

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -11,13 +11,18 @@
       @foreach($allNews as $newsItem)
         <div class="row">
           <div class="col-lg col-sm-12 d-flex flex-column justify-content-center news-item-content">
+            <h4 class="main-header mb-3">{{ $newsItem->title }}</h4>
             {!! $newsItem->description !!}
-            <p class="small main-text">Created: {{ date_format($newsItem->created_at, "d/m/Y") }}</p>
+            <p class="small main-text">{{ date_format($newsItem->created_at, "d/m/Y") }}</p>
           </div>
-          <div class="col-lg col-sm-12 d-flex flex-column align-items-lg-end align-items-center">
+          <div class="col-lg col-sm-12 d-flex flex-column align-items-lg-end align-items-center justify-content-center">
+            <div class="row justify-content-lg-end justify-content-center">
             @foreach($newsItem->images as $image)
-              <img src="{{$image['image_name']}}" width="300" class="img-fluid my-2" alt="">
+              <div class="col-sm-6 col-12 d-flex justify-content-center">
+                <img src="{{$image['image_name']}}" class="img-fluid my-2" alt="">
+              </div>
             @endforeach
+            </div>
           </div>
         </div>
         <hr>
