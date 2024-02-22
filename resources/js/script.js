62f4e12d05791fc5695a0d26e7272a144565d1c7
Linards <9407839+llinards@users.noreply.github.com>
2024-02-22 14:07:53 +0200

    script js cleanup

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,31 +1,6 @@
 // change background for navbar when scrolling down
 $(function () {
-  // handle links with @href started with '#' only
-  $(document).on("click", 'a[href^="#"]', function (e) {
-    // target element id
-    var id = $(this).attr("href");
 
-    // target element
-    var $id = $(id);
-    if ($id.length === 0) {
-      return;
-    }
-
-    // prevent standard hash navigation (avoid blinking in IE)
-    e.preventDefault();
-
-    // top position relative to the document
-    var pos = $id.offset().top;
-
-    // animated top scrolling
-    $("body, html")
-      .animate({
-        scrollTop: pos
-      })
-      .offset();
-  });
-
-  //
   var btn = $("#button-up");
   $(window).scroll(function () {
     if ($(window).scrollTop() > 100) {
@@ -45,11 +20,7 @@ $(function () {
     );
   });
   //
-  $(document).on("click", ".navbar-collapse", function (e) {
-    if ($(e.target).is("a")) {
-      $(this).collapse("hide");
-    }
-  });
+
 
   $(document).on("click", ".deleteImage", function () {
     var imageId = $(this).attr("data-imageid");
@@ -57,12 +28,6 @@ $(function () {
     $("#deleteModal").modal("show");
   });
 
-  $(document).on("click", '[data-toggle="lightbox"]', function (event) {
-    event.preventDefault();
-    $(this).ekkoLightbox({
-      alwaysShowClose: false
-    });
-  });
 
   const menu = document.querySelector(".navbar-toggler") || null;
   if (menu != null) {
