f0280bcb21d0e2d2ef2f35658522bcda72defb5a
Linards <9407839+llinards@users.noreply.github.com>
2024-03-26 21:35:47 +0200

    Add temporary file upload tests

---
+++ tests/Feature/SingleImageUploadTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Storage;
+use Tests\TestCase;
+
+class SingleImageUploadTest extends TestCase
+{
+  use RefreshDatabase;
+
+  private User $user;
+
+  protected function setUp(): void
+  {
+    parent::setUp();
+    $this->user = User::factory()->create();
+  }
+
+  public function test_single_image_temporary_upload(): void
+  {
+    Storage::fake('public');
+    $this->actingAs($this->user);
+    $response = $this->post(route('image_temporary.store'), [
+      'single-image' => UploadedFile::fake()->image('single-file.jpg')
+    ]);
+
+    $response->assertStatus(200);
+
+    Storage::disk('public')->assertExists($response->content());
+  }
+
+  public function test_multiple_image_temporary_upload(): void
+  {
+    Storage::fake('public');
+    $this->actingAs($this->user);
+
+    $response = $this->post(route('image_temporary.store'), [
+      'multiple-images' => [
+        UploadedFile::fake()->image('multiple-file-one.jpg'),
+        UploadedFile::fake()->image('multiple-file-two.jpg')
+      ],
+    ]);
+
+    $response->assertStatus(200);
+    Storage::disk('public')->assertExists($response->content());
+  }
+}
