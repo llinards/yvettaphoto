b8532b0e2f0fee5b3895edf27d8baf2886994fac
Linards <9407839+llinards@users.noreply.github.com>
2024-04-03 21:01:35 +0300

    Optimize and update news tests

--- tests/Feature/NewsTest.php
+++ tests/Feature/NewsTest.php
@@ -13,17 +13,16 @@ class NewsTest extends TestCase
 {
   use RefreshDatabase;
 
-  private User $user;
-
   protected function setUp(): void
   {
     parent::setUp();
-    $this->user = User::factory()->create();
+    $user = User::factory()->create();
+    $this->actingAs($user);
   }
 
   public function test_news_index_can_be_opened(): void
   {
-    $this->actingAs($this->user);
+
     $response = $this->get(route('news.index'));
 
     $response->assertStatus(200);
@@ -31,7 +30,7 @@ class NewsTest extends TestCase
 
   public function test_create_news_can_be_opened(): void
   {
-    $this->actingAs($this->user);
+
     $response = $this->get(route('news.create'));
 
     $response->assertStatus(200);
@@ -39,35 +38,49 @@ class NewsTest extends TestCase
 
   public function test_news_can_be_created(): void
   {
-    Storage::fake('public');
-    $this->actingAs($this->user);
-    $file = $this->uploadMultipleImages(UploadedFile::fake()->image('test.jpg', 1920, 1080));
-    $response = $this->post(route('news.store'), [
-      'news-title' => 'Test News',
-      'description-textarea' => 'Description for test news',
-      'multiple-images' => [$file]
-    ]);
+    {
+      Storage::fake('public');
+      $files = [
+        UploadedFile::fake()->image('test1.jpg', 1920, 1080),
+        UploadedFile::fake()->image('test2.jpg', 1920, 1080)
+      ];
 
-    $response->assertSessionHas([
-      'success' => 'Ziņa pievienota!'
-    ]);
+      $uploadedFiles = [];
+      foreach ($files as $file) {
+        $uploadedFiles[] = $this->uploadMultipleImages($file);
+      }
 
-    Storage::disk('public')->assertExists('uploads/news/'.basename($file));
+      $response = $this->post(route('news.store'), [
+        'news-title' => 'Test News',
+        'description-textarea' => 'Description for test news',
+        'multiple-images' => $uploadedFiles
+      ]);
 
-    $this->assertDatabaseHas('news', [
-      'title' => 'Test News',
-      'description' => 'Description for test news'
-    ]);
+      $response->assertSessionHas([
+        'success' => 'Ziņa pievienota!'
+      ]);
 
-    $this->assertDatabaseHas('news_images', [
-      'news_id' => News::first()->id,
-      'image_location' => basename($file),
-    ]);
+      foreach ($uploadedFiles as $uploadedFile) {
+        Storage::disk('public')->assertExists('uploads/news/'.basename($uploadedFile));
+      }
+
+      $this->assertDatabaseHas('news', [
+        'title' => 'Test News',
+        'description' => 'Description for test news'
+      ]);
+
+      foreach ($uploadedFiles as $uploadedFile) {
+        $this->assertDatabaseHas('news_images', [
+          'news_id' => News::first()->id,
+          'image_location' => basename($uploadedFile),
+        ]);
+      }
+    }
   }
 
   public function test_validation_when_creating_news(): void
   {
-    $this->actingAs($this->user);
+
     $response = $this->post(route('news.store'), [
       'news-title' => '',
       'description-textarea' => '',
@@ -83,7 +96,7 @@ class NewsTest extends TestCase
 
   public function test_validation_when_title_is_longer_than_100_characters(): void
   {
-    $this->actingAs($this->user);
+
     $response = $this->post(route('news.store'), [
       'news-title' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,',
     ]);
@@ -95,21 +108,21 @@ class NewsTest extends TestCase
 
   public function test_created_news_visible_on_news_page(): void
   {
-    //TODO: Double check if this doesn't give false positive
     Storage::fake('public');
-    News::factory(1)->create();
-    $news = News::first();
+    $news = News::factory()->create();
     $response = $this->get('/news');
 
+    $response->assertStatus(200);
     $response->assertSeeText($news->title);
+    $response->assertSeeText($news->description);
   }
-
 
   private function uploadMultipleImages(UploadedFile $file): string
   {
     $response = $this->post(route('image_temporary.store'), [
       'multiple-images' => [$file],
     ]);
+    $response->assertStatus(200);
     return $response->content();
   }
 }
