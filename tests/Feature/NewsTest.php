71cf570e90d3dc79741b92046b7754a7f055e07e
Linards <9407839+llinards@users.noreply.github.com>
2024-04-05 15:28:43 +0300

    Add remaining tests for News

--- tests/Feature/NewsTest.php
+++ tests/Feature/NewsTest.php
@@ -38,43 +38,41 @@ class NewsTest extends TestCase
 
   public function test_news_can_be_created(): void
   {
-    {
-      Storage::fake('public');
-      $files = [
-        UploadedFile::fake()->image('test1.jpg', 1920, 1080),
-        UploadedFile::fake()->image('test2.jpg', 1920, 1080)
-      ];
+    Storage::fake('public');
+    $files = [
+      UploadedFile::fake()->image('test1.jpg', 1920, 1080),
+      UploadedFile::fake()->image('test2.jpg', 1920, 1080)
+    ];
 
-      $uploadedFiles = [];
-      foreach ($files as $file) {
-        $uploadedFiles[] = $this->uploadMultipleImages($file);
-      }
+    $uploadedFiles = [];
+    foreach ($files as $file) {
+      $uploadedFiles[] = $this->uploadMultipleImages($file);
+    }
 
-      $response = $this->post(route('news.store'), [
-        'news-title' => 'Test News',
-        'description-textarea' => 'Description for test news',
-        'multiple-images' => $uploadedFiles
+    $response = $this->post(route('news.store'), [
+      'news-title' => 'Test News',
+      'description-textarea' => 'Description for test news',
+      'multiple-images' => $uploadedFiles
+    ]);
+
+    $response->assertSessionHas([
+      'success' => 'Ziņa pievienota!'
+    ]);
+
+    foreach ($uploadedFiles as $uploadedFile) {
+      Storage::disk('public')->assertExists('uploads/news/'.basename($uploadedFile));
+    }
+
+    $this->assertDatabaseHas('news', [
+      'title' => 'Test News',
+      'description' => 'Description for test news'
+    ]);
+
+    foreach ($uploadedFiles as $uploadedFile) {
+      $this->assertDatabaseHas('news_images', [
+        'news_id' => News::first()->id,
+        'image_location' => basename($uploadedFile),
       ]);
-
-      $response->assertSessionHas([
-        'success' => 'Ziņa pievienota!'
-      ]);
-
-      foreach ($uploadedFiles as $uploadedFile) {
-        Storage::disk('public')->assertExists('uploads/news/'.basename($uploadedFile));
-      }
-
-      $this->assertDatabaseHas('news', [
-        'title' => 'Test News',
-        'description' => 'Description for test news'
-      ]);
-
-      foreach ($uploadedFiles as $uploadedFile) {
-        $this->assertDatabaseHas('news_images', [
-          'news_id' => News::first()->id,
-          'image_location' => basename($uploadedFile),
-        ]);
-      }
     }
   }
 
@@ -115,6 +113,124 @@ class NewsTest extends TestCase
     $response->assertStatus(200);
     $response->assertSeeText($news->title);
     $response->assertSeeText($news->description);
+  }
+
+  public function test_edit_news_can_be_opened(): void
+  {
+    $news = News::factory()->create();
+    $response = $this->get(route('news.edit', $news));
+
+    $response->assertStatus(200);
+  }
+
+  public function test_news_can_be_updated(): void
+  {
+    Storage::fake('public');
+    $news = News::factory()->create();
+    $files = [
+      UploadedFile::fake()->image('test1.jpg', 1920, 1080),
+      UploadedFile::fake()->image('test2.jpg', 1920, 1080)
+    ];
+
+    $uploadedFiles = [];
+    foreach ($files as $file) {
+      $uploadedFiles[] = $this->uploadMultipleImages($file);
+    }
+
+    $response = $this->patch(route('news.update', $news), [
+      'news-id' => $news->id,
+      'news-title' => 'Updated News',
+      'description-textarea' => 'Updated description for test news',
+      'multiple-images' => $uploadedFiles
+    ]);
+
+    $response->assertSessionHas([
+      'success' => 'Ziņa atjaunota!'
+    ]);
+
+    foreach ($uploadedFiles as $uploadedFile) {
+      Storage::disk('public')->assertExists('uploads/news/'.basename($uploadedFile));
+    }
+
+    $this->assertDatabaseHas('news', [
+      'id' => $news->id,
+      'title' => 'Updated News',
+      'description' => 'Updated description for test news'
+    ]);
+
+    foreach ($uploadedFiles as $uploadedFile) {
+      $this->assertDatabaseHas('news_images', [
+        'news_id' => $news->id,
+        'image_location' => basename($uploadedFile),
+      ]);
+    }
+  }
+
+  public function test_validation_when_updating_news(): void
+  {
+    $news = News::factory()->create();
+
+    $response = $this->patch(route('news.update', $news), [
+      'news-id' => $news->id,
+      'news-title' => '',
+      'description-textarea' => '',
+    ]);
+
+    $response->assertSessionHasErrors([
+      'news-title' => 'Nav ievadīts virsraksts!',
+      'description-textarea' => 'Nav ievadīts teksts!',
+    ]);
+
+  }
+
+  public function test_image_can_be_deleted_from_news(): void
+  {
+    Storage::fake('public');
+    $news = News::factory()->create();
+    $image = $news->images()->create([
+      'image_location' => 'test.jpg'
+    ]);
+
+    $response = $this->delete(route('news.image.destroy', $image));
+
+    $response->assertSessionHas([
+      'success' => 'Bilde dzēsta'
+    ]);
+
+    Storage::disk('public')->assertMissing('uploads/news/test.jpg');
+
+    $this->assertDatabaseMissing('news_images', [
+      'id' => $image->id,
+      'image_location' => 'test.jpg'
+    ]);
+  }
+
+  public function test_news_can_be_deleted(): void
+  {
+    Storage::fake('public');
+    $news = News::factory()->create();
+    $image = $news->images()->create([
+      'image_location' => 'test.jpg'
+    ]);
+
+    $response = $this->delete(route('news.destroy', $news));
+
+    $response->assertSessionHas([
+      'success' => 'Ziņa izdzēsta!'
+    ]);
+
+    Storage::disk('public')->assertMissing('uploads/news/test.jpg');
+
+    $this->assertDatabaseMissing('news', [
+      'id' => $news->id,
+      'title' => $news->title,
+      'description' => $news->description
+    ]);
+
+    $this->assertDatabaseMissing('news_images', [
+      'id' => $image->id,
+      'image_location' => 'test.jpg'
+    ]);
   }
 
   private function uploadMultipleImages(UploadedFile $file): string
