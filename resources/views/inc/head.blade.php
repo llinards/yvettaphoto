d448a6380bc5ac5e0d91facb1b6f422d9ab497c6
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-05-23 14:48:39 +0300

    Update GA

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -2,20 +2,26 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122667456-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+@if(App::environment('production'))
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-K6WG6G2EWG"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'UA-122667456-1');
-</script>
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'G-K6WG6G2EWG');
+  </script>
+@endif
 <title>
-	@isset($title)
-		{{ $title }} |
-	@endisset
-	{{ config('app.name') }}
+  @isset($title)
+    {{ $title }} |
+  @endisset
+  {{ config('app.name') }}
 </title>
 @include('inc.css')
 @include('inc.js')
